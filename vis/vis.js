677efb15e1e765bff72f90a80375acf7807626ec
George Field <gfield145@gmail.com>
2018-03-02 23:15:25 +0000

    Graph all fund values with stacked areas.

--- vis/vis.js
+++ vis/vis.js
@@ -4,21 +4,27 @@ var hist = require('../history.json');
 
 var ctx = document.getElementById('fundchart');
 
+// Colour palette courtesy of http://www.colorhunt.co/c/107141
+let colors = ['#4a772f', '#ffdd00', '#fa9e05', '#a7095c'];
+
+function getFunds(hist) {
+    return Object.keys(hist[0].funds);
+}
+
 function unpackFundTrend(hist, name) {
     return hist.map((d,i) => {
         return { x: d.timestamp, y: d.funds[name].value };
     });
 }
 
-let mydat = unpackFundTrend(hist, 'fundA');
+datasets = getFunds(hist).map((name, i) => {
+    return { label: name, data: unpackFundTrend(hist, name), backgroundColor: colors[i], pointBorderColor: '#000000' };
+});
 
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        datasets: [{
-            label: "fundA",
-            data: mydat,
-        }]
+        datasets: datasets,
     },
     options: {
         scales: {
@@ -32,6 +38,7 @@ var myChart = new Chart(ctx, {
             }],
             yAxes: [{
                 display: true,
+                stacked: true,
                 scaleLabel: {
                     display: true,
                     labelString: 'Fund Value',
